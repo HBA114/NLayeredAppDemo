b5b6875a10c90ecc6e05fa07f2800c5246b4c7e0
HBA114 <hbasriayhaner114@gmail.com>
2023-02-08 17:29:12 +0300

    Deleted Commented Code ...
    Removed unnecessary project reference from api layer

--- Northwind.DataAccess/Concrete/ProductDal.cs
+++ Northwind.DataAccess/Concrete/ProductDal.cs
@@ -14,47 +14,29 @@ public class ProductDal
 
     public List<Product> GetAll()
     {
-        // using (NorthwindContext context = new NorthwindContext())
-        // {
-        //     return context.Products.ToList();
-        // }
-
         return context.Products.ToList();
     }
 
     public Product Get(int id)
     {
-        // using (NorthwindContext context = new NorthwindContext())
-        // {
-        //     return context.Products.SingleOrDefault(p => p.ProductId == id)!;
-        // }
         return context.Products.SingleOrDefault(p => p.ProductId == id)!;
     }
 
     public void Add(Product product)
     {
-        // using (NorthwindContext context = new NorthwindContext())
-        // {
-        //     context.Products.Add(product);
-        //     context.SaveChanges();
-        // }
+        context.Products.Add(product);
+        context.SaveChanges();
     }
 
     public void Update(Product product)
     {
-        // using (NorthwindContext context = new NorthwindContext())
-        // {
-        //     // context.Products.Add(product);
-        //     context.SaveChanges();
-        // }
+        // context.Products.Add(product);
+        context.SaveChanges();
     }
 
     public void Delete(Product product)
     {
-        // using (NorthwindContext context = new NorthwindContext())
-        // {
         //     context.Products.Remove(product);
         //     context.SaveChanges();
-        // }
     }
 }
